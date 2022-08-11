2ea5de1855037eece588d30d4cec0a4aa77f184e
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-11 22:17:09 +0500

    Added sending to email

--- app/src/main/java/com/shevy/androidcourse/OrderActivity.kt
+++ app/src/main/java/com/shevy/androidcourse/OrderActivity.kt
@@ -1,12 +1,16 @@
 package com.shevy.androidcourse
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.shevy.androidcourse.databinding.ActivityOrderBinding
 
 class OrderActivity : AppCompatActivity() {
     lateinit var binding: ActivityOrderBinding
+    private var addresses: String = "dmitriy_shevyakov@mail.ru"
+    private var subject: String = "Order from Music Shop"
+    lateinit var emailText: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,18 +22,29 @@ class OrderActivity : AppCompatActivity() {
         val goodsName: String = receivedOrderIntent.getStringExtra("goodsName").toString()
         val quantity: Int = receivedOrderIntent.getIntExtra("quantity", 0)
         val orderPrice: Double = receivedOrderIntent.getDoubleExtra("orderPrice", 0.0)
+
+        emailText = "Customer name: $userName \n" +
+                "Goods name: $goodsName \n" +
+                "Quantity: $quantity \n" +
+                "Order price: $orderPrice"
+
         binding.apply {
-            orderTextView.text = "Customer name: $userName \n" +
-                    "Goods name: $goodsName \n" +
-                    "Quantity: $quantity \n" +
-                    "Order price: $orderPrice"
+            orderTextView.text = emailText
 /*            orderTextView.text = userName
             orderTextView2.text = goodsName
             orderTextView3.text = quantity.toString()
             orderTextView4.text = orderPrice.toString()*/
 
             sendButton.setOnClickListener {
-
+                val intent = Intent(Intent.ACTION_SENDTO).apply {
+                    data = Uri.parse("mailto:") // only email apps should handle this
+                    putExtra(Intent.EXTRA_EMAIL, addresses)
+                    putExtra(Intent.EXTRA_SUBJECT, subject)
+                    putExtra(Intent.EXTRA_TEXT, emailText)
+                }
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
             }
         }
     }
