ae279dc00846e23ecf41a4447fd75a3368b6128f
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-13 10:40:33 +0500

    Added Tom & Jerry animation

--- app/src/main/java/com/shevy/androidcourse/MainActivity.kt
+++ app/src/main/java/com/shevy/androidcourse/MainActivity.kt
@@ -11,86 +11,27 @@ import androidx.appcompat.app.AppCompatActivity
 import com.shevy.androidcourse.databinding.ActivityMainBinding
 import kotlin.properties.Delegates
 
-class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
-    var quantity = 0
+class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    private lateinit var spinner: Spinner
-    lateinit var goodsMap: MutableMap<String, Double>
-    lateinit var goodsName: String
-    lateinit var userNameEditText: EditText
-    var price by Delegates.notNull<Double>()
+    var isTomVisible = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        userNameEditText = binding.nameEditText
-        spinner = binding.spinner
-        val spinnerArrayList = arrayListOf<String>()
+        binding.imageViewTom.setOnClickListener {
+            if (isTomVisible) {
+                binding.imageViewTom.animate().alpha(0f).duration = 3000
+                binding.imageViewJerry.animate().alpha(1f).duration = 3000
+                isTomVisible = false
+            } else {
 
-        createSpinner(spinnerArrayList)
+                binding.imageViewJerry.animate().alpha(0f).duration = 3000
+                binding.imageViewTom.animate().alpha(1f).duration = 3000
+                isTomVisible = true
 
-        val spinnerAdapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_spinner_item,
-            spinnerArrayList
-        )
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = spinnerAdapter
-
-        goodsMap = mutableMapOf()
-        goodsMap["guitar"] = 500.0
-        goodsMap["drums"] = 1500.0
-        goodsMap["keyboard"] = 1000.0
-
-        binding.apply {
-            plusButton.setOnClickListener {
-                quantity++
-                quantityCountTextView.text = quantity.toString()
-                binding.priceTextView.text = (quantity * price).toString()
             }
-            minusButton.setOnClickListener {
-                quantity--
-                if (quantity < 0) quantity = 0
-                quantityCountTextView.text = quantity.toString()
-                binding.priceTextView.text = (quantity * price).toString()
-            }
-            addToCartBtn.setOnClickListener {
-                val order = Order()
-                order.userName = userNameEditText.text.toString()
-                order.goodsName = goodsName
-                order.quantity = quantity
-                order.orderPrice = quantity * price
-
-                val orderIntent = Intent(this@MainActivity, OrderActivity::class.java)
-                orderIntent.putExtra("userNameForIntent", order.userName)
-                orderIntent.putExtra("goodsName", order.goodsName)
-                orderIntent.putExtra("quantity", order.quantity)
-                orderIntent.putExtra("orderPrice", order.orderPrice)
-                startActivity(orderIntent)
-            }
-            spinner.onItemSelectedListener = this@MainActivity
         }
-    }
-
-    private fun createSpinner(spinnerArrayList: ArrayList<String>) {
-        spinnerArrayList.add("guitar")
-        spinnerArrayList.add("drums")
-        spinnerArrayList.add("keyboard")
-    }
-
-    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        goodsName = spinner.selectedItem.toString()
-        price = goodsMap[goodsName]!!
-        binding.priceTextView.text = (quantity * price).toString()
-        when (goodsName) {
-            "guitar" -> binding.goodsImageView.setImageResource(R.drawable.guitar)
-            "drums" -> binding.goodsImageView.setImageResource(R.drawable.drums)
-            "keyboard" -> binding.goodsImageView.setImageResource(R.drawable.keyboard)
-        }
-    }
-
-    override fun onNothingSelected(p0: AdapterView<*>?) {
     }
 }
--- app/src/main/java/com/shevy/androidcourse/Order.kt
+++
@@ -1,9 +0,0 @@
-package com.shevy.androidcourse
-
-class Order {
-    var userName: String = ""
-    var goodsName: String = ""
-    var quantity: Int = 0
-    var orderPrice: Double = 0.0
-
-}
