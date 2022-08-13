00062c65cc2abeb443e0f6109dc89c7450ada484
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-13 12:02:15 +0500

    Added videoView

--- app/src/main/java/com/shevy/androidcourse/MainActivity.kt
+++ app/src/main/java/com/shevy/androidcourse/MainActivity.kt
@@ -1,35 +1,30 @@
 package com.shevy.androidcourse
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.Spinner
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.shevy.androidcourse.databinding.ActivityMainBinding
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    var isTomVisible = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.imageViewTom.setOnClickListener {
-            if (isTomVisible) {
-                binding.imageViewTom.animate().alpha(0f).duration = 3000
-                binding.imageViewJerry.animate().alpha(1f).duration = 3000
-                isTomVisible = false
-            } else {
-                binding.imageViewJerry.animate().alpha(0f).duration = 3000
-                binding.imageViewTom.animate().alpha(1f).duration = 3000
-                isTomVisible = true
-            }
-        }
+        val videoView = binding.videoView
+        //videoView.setVideoURI(Uri.parse("https://www.youtube.com/watch?v=QnUed5I4-2E"))
+        videoView.setVideoPath("android.resource://" + packageName + "/" + R.raw.video)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+
+        videoView.start()
+
     }
 }
