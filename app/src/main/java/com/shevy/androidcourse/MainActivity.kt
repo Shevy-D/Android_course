1d8cf6ac8d21ecf2ee0443e9d95c5c0c1e39289a
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-13 20:28:36 +0500

    Added audio plater

--- app/src/main/java/com/shevy/androidcourse/MainActivity.kt
+++ app/src/main/java/com/shevy/androidcourse/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.shevy.androidcourse
 
 import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
@@ -11,20 +13,46 @@ import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private lateinit var mediaPlayer: MediaPlayer
+    lateinit var volumeSeekBar: SeekBar
+    lateinit var audioManager: AudioManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val videoView = binding.videoView
-        //videoView.setVideoURI(Uri.parse("https://www.youtube.com/watch?v=QnUed5I4-2E"))
-        videoView.setVideoPath("android.resource://" + packageName + "/" + R.raw.video)
-        val mediaController = MediaController(this)
-        mediaController.setAnchorView(videoView)
-        videoView.setMediaController(mediaController)
+        audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
 
-        videoView.start()
+        volumeSeekBar = binding.seekBarVolume
+        volumeSeekBar.max = maxVolume
+        volumeSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
+            }
 
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                //TODO("Not yet implemented")
+            }
+        })
+        mediaPlayer = MediaPlayer.create(this, R.raw.music)
+
+        binding.apply {
+            playImageView.setOnClickListener {
+                if (mediaPlayer.isPlaying) {
+                    mediaPlayer.pause()
+                    playImageView.setImageResource(R.drawable.ic_play)
+                } else {
+                    mediaPlayer.start()
+                    playImageView.setImageResource(R.drawable.ic_pause)
+                }
+            }
+        }
     }
 }
+
