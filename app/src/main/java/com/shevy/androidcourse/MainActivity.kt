bf89f416ee8aa49f29cedcb27e71c7f7e15e8041
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-13 20:52:53 +0500

    Added audio player

--- app/src/main/java/com/shevy/androidcourse/MainActivity.kt
+++ app/src/main/java/com/shevy/androidcourse/MainActivity.kt
@@ -9,12 +9,14 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.shevy.androidcourse.databinding.ActivityMainBinding
+import java.util.*
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     private lateinit var mediaPlayer: MediaPlayer
-    lateinit var volumeSeekBar: SeekBar
+    private lateinit var volumeSeekBar: SeekBar
+    lateinit var timerSeekBar: SeekBar
     lateinit var audioManager: AudioManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         volumeSeekBar = binding.seekBarVolume
         volumeSeekBar.max = maxVolume
-        volumeSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        volumeSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
             }
@@ -42,6 +44,30 @@ class MainActivity : AppCompatActivity() {
         })
         mediaPlayer = MediaPlayer.create(this, R.raw.music)
 
+        timerSeekBar = binding.seekBarTimer
+        timerSeekBar.max = mediaPlayer.duration
+        timerSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                //TODO("Not yet implemented")
+            }
+        })
+
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                timerSeekBar.progress = mediaPlayer.currentPosition
+            }
+        }, 0, 1000)
+
         binding.apply {
             playImageView.setOnClickListener {
                 if (mediaPlayer.isPlaying) {
@@ -51,6 +77,14 @@ class MainActivity : AppCompatActivity() {
                     mediaPlayer.start()
                     playImageView.setImageResource(R.drawable.ic_pause)
                 }
+            }
+
+            nextImageView.setOnClickListener {
+
+            }
+
+            previousImageView.setOnClickListener {
+
             }
         }
     }
